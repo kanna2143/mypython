fca6fc031e78584e8c5c243b364c5c3421bd26a6
kanna2143 <murthypramila0@gmail.com>
2025-11-12 15:56:22 +0530

    Create #write a Python function called add_numbers() that takes two numbers as arguments .py

---
+++ #write a Python function called add_numbers() that takes two numbers as arguments .py
@@ -0,0 +1,6 @@
+#Write a Python function called add_numbers() that takes two numbers as arguments and returns their sum
+def add_numbers(a, b):
+    return a + b
+
+# Example:
+print(add_numbers(25, 22))
