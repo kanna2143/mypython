442b08163dde72fed4471cca7f5ee105f82ec0ab
kanna2143 <murthypramila0@gmail.com>
2025-11-12 20:03:05 +0530

    Implement function to compare two numbers

---
+++ function largest_f_twe.py
@@ -0,0 +1,7 @@
+def largest_f_tw(a, b):
+    if a > b:
+        print(f"{a} is larger than {b}")
+    elif b > a:
+        print(f"{b} is larger than {a}")
+    else:
+        print("Both numbers are equal")
